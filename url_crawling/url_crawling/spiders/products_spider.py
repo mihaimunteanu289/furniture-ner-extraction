135d40272849237b5072e9d4da046657d526fd31
unknown <mihighgh28@gmail.com>
2023-06-13 20:49:20 +0300

    minor updates

--- url_crawling/url_crawling/spiders/products_spider.py
+++ url_crawling/url_crawling/spiders/products_spider.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from typing import Generator
 from scrapy.http import Response, Request
 from scrapy import Spider, Request, Item, Field
 from scrapy.linkextractors import LinkExtractor
@@ -14,10 +13,10 @@ class Product(Item):
     url = Field()
     error = Field()
 
-# Defining a new spider 'ProductSpider'
+# Defining a new spider called 'ProductSpider'
 class ProductSpider(Spider):
     name = 'ProductSpider'
-    link_extractor = LinkExtractor(allow=r'/products/(.+)') # Regex for extracting product URLs
+    link_extractor = LinkExtractor(allow=r'/products/(.+)')
 
     # Setting the output format, encoding, and overwrite option for the Scrapy feed
     custom_settings = {
@@ -31,33 +30,30 @@ class ProductSpider(Spider):
         'ROBOTSTXT_OBEY': False
     }
 
-
-    # This function reads the CSV file and starts the request process
-    def start_requests(self) -> Generator[Request, None, None]:
+    def start_requests(self):
         try:
-            ds = pd.read_csv(CSV_PATH) # Reading the CSV file
-            products = ds.iloc[:, 0].tolist() # Get the first two product URLs from the CSV
+            ds = pd.read_csv(CSV_PATH)
+            products = ds.iloc[:, 0].tolist()
         except Exception as e:
             # Log error message if there's a problem reading the CSV
             self.logger.error(f"Error reading CSV: {str(e)}")
             return
 
-        # Make requests for each product URL and call 'parse_product_list' function when a response is received
+        # Make requests for each product URL and call 'parse_product_list' if we get a response
         for product_url in products:
             yield Request(url=product_url, callback=self.parse_product_list, errback=self.handle_error)
 
     # This function extracts product URLs from the received response and initiates requests to them
-    def parse_product_list(self, response: Response) -> Generator[Request, None, None]:
-        product_links = self.link_extractor.extract_links(response) # Extract all product links
+    def parse_product_list(self, response):
+        product_links = self.link_extractor.extract_links(response)
 
-        # Make requests for each extracted product link and call 'parse_product' function when a response is received
         for link in product_links:
             # Skip URLs that end with "/"
             if not link.url.endswith('/'):
                 yield Request(url=link.url, callback=self.parse_product, errback=self.handle_error)
 
     # This function extracts the product name from the received response
-    def parse_product(self, response: Response) -> Generator[Product, None, None]:
+    def parse_product(self, response):
         product_name = response.css('h1::text').get()
         # product_name = response.xpath("//meta[@property='og:title']/@content").get()
         if product_name:
@@ -68,6 +64,5 @@ class ProductSpider(Spider):
             yield Product(url=response.url, error="No product name found")
             
     # This function handles any errors that occur during the request process
-    def handle_error(self, failure) -> Generator[Product, None, None]:
-        # If an error occurs, yield a new Product item with the URL and error message
+    def handle_error(self, failure):
         yield Product(url=failure.request.url, error=str(failure.value))
